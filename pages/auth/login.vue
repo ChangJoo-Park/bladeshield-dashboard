e7ce27681d1dab607b8818c963071258355fcac8
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-11 22:37:31 +0900

    Add Login page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    Hello World
-  </div>
+  <v-layout align-center>
+    <v-flex xs12 sm4 text-xs-center>
+      <div>
+        <v-btn color="error">Login With Google</v-btn>
+      </div>
+      <div>
+        <v-btn color="primary">Login With Github</v-btn>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-
+  layout: 'full'
 }
 </script>
 
