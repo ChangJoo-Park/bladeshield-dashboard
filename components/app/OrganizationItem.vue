8d732a6c6ee4e6b22d711bac3d49b02c2dbbde01
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-12 12:11:31 +0900

    Add organization item component

---
+++ components/app/OrganizationItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-flex xs12>
+    <v-card color="blue-grey" class="white--text">
+      <v-card-title primary-title>
+        <div class="headline">{{ item.name }}&nbsp;</div>
+        <div>{{ item.description }}</div>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn
+          flat
+          dark
+          :to="{ name: 'organization_name', params: { organization_name: item.slug } }">
+          열기
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
