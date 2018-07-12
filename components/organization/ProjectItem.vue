163c7aa873303ae29d6793c669d2eac41987ea9f
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-12 12:11:36 +0900

    Add project item component

---
+++ components/organization/ProjectItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-flex xs12>
+    <v-card>
+      <v-card-title primary-title>
+        <div class="headline">{{ item.name }}&nbsp;</div>
+        <div>{{ item.description }}</div>
+      </v-card-title>
+      <v-card-actions>
+        <v-btn
+          flat
+          :to="{
+            name: 'organization_name-project_name',
+            params: {
+              organization_name: 'hello-world',
+              project_name: 'project-name'
+            }
+          }">
+          열기
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
