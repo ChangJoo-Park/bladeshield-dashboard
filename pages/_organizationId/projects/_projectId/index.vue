b3a121e617b0a02dadbbe78a5fcbd89ed9b08df9
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-15 20:46:56 +0900

    Update project details. using API

--- pages/_organizationId/projects/_projectId/index.vue
+++ pages/_organizationId/projects/_projectId/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <h1>프로젝트 이름</h1>
+    <h1>{{ project.name }}</h1>
+    <small>{{ project.description }}</small>
     <v-card>
       <v-card-title>
-        Nutrition
+        Issues
         <v-spacer></v-spacer>
         <v-text-field
           v-model="search"
@@ -15,7 +16,7 @@
       </v-card-title>
       <v-data-table
         :headers="headers"
-        :items="desserts"
+        :items="project.issues"
         select-all
         class="elevation-1"
       >
@@ -42,115 +43,24 @@
 <script>
 export default {
   async asyncData ({ app, route }) {
-    return {}
+    const { params: { organizationId, projectId } } = route
+    const project = await app.$axios.$get(`/api/projects/${projectId}?organization=${organizationId}`)
+    return { project }
   },
   data () {
     return {
       search: '',
       headers: [
         {
-          text: 'Dessert (100g serving)',
+          text: 'Error Message',
           align: 'left',
           sortable: false,
-          value: 'name'
+          value: 'title'
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' }
-      ],
-      desserts: [
-        {
-          value: false,
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%'
-        },
-        {
-          value: false,
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%'
-        },
-        {
-          value: false,
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%'
-        },
-        {
-          value: false,
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%'
-        },
-        {
-          value: false,
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%'
-        },
-        {
-          value: false,
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%'
-        },
-        {
-          value: false,
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%'
-        },
-        {
-          value: false,
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%'
-        },
-        {
-          value: false,
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%'
-        },
-        {
-          value: false,
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
-        }
+        { text: 'Source', value: 'source' },
+        { text: 'Message', value: 'message' },
+        { text: 'Events', value: 'events' },
+        { text: 'Users', value: 'users' }
       ]
     }
   },
@@ -162,7 +72,7 @@ export default {
         params: {
           organizationId,
           projectId,
-          issueId: '11'
+          issueId: item._id
         }
       })
     }
