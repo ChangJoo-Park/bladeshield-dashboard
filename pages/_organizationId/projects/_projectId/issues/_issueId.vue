ce8de474cc454d7a328e5870751f7006f1c1ef21
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-19 04:25:17 +0900

    Add fetch issue from id

--- pages/_organizationId/projects/_projectId/issues/_issueId.vue
+++ pages/_organizationId/projects/_projectId/issues/_issueId.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <h1>Issue Number</h1>
+    <h1>{{ issue }}</h1>
   </div>
 </template>
 
 <script>
 export default {
+  async asyncData ({ app, route }) {
+    const { params: { organizationId, projectId, issueId } } = route
+    const issue = await app.$axios.$get(`/api/issues/${issueId}`)
 
+    return { issue }
+  }
 }
 </script>
 
