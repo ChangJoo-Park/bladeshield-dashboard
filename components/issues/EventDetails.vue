e92115357ed94ca3db7aa967a8988de9440da768
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-19 05:35:21 +0900

    Add event pager to event details, using currentEvent

--- components/issues/EventDetails.vue
+++ components/issues/EventDetails.vue
@@ -1,14 +1,43 @@
 <template>
   <v-tab-item id="tab-details">
     <v-card>
-      <v-card-text>Event Details</v-card-text>
+      <pager
+        @updated-index="onUpdateCurrentIndex"
+        :total="events.length"
+      />
+      <h3>Event : {{ currentEvent._id }}</h3>
+      <h4>{{ currentEvent.createdAt }}</h4>
+      <v-card-text>{{ currentEvent }}</v-card-text>
     </v-card>
   </v-tab-item>
 </template>
 
 <script>
+import Pager from '~/components/issues/event-details/EventPager.vue'
 export default {
-
+  components: {
+    Pager
+  },
+  props: {
+    events: {
+      types: Array,
+      required: true,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      currentEvent: null
+    }
+  },
+  created () {
+    this.currentEvent = this.events[0]
+  },
+  methods: {
+    onUpdateCurrentIndex (index) {
+      this.currentEvent = this.events[index]
+    }
+  }
 }
 </script>
 
