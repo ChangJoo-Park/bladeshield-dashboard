6e8a5d62533d72cdb159c4429b85c205d31eac13
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-19 05:34:57 +0900

    Add event pager

--- components/issues/event-details/EventPager.vue
+++ components/issues/event-details/EventPager.vue
@@ -1,12 +1,71 @@
 <template>
   <div>
-
+    <v-btn
+      small
+      ripple
+      @click="updateIndex(0)"
+      :disabled="isFirstEvent"
+    >
+      &lt;|
+    </v-btn>
+    <v-btn
+      small
+      ripple
+      :disabled="!isRemainOlder"
+    >
+      Older
+    </v-btn>
+    <v-btn
+      small
+      ripple
+      :disabled="!isRemainNewer"
+    >
+      Newer
+    </v-btn>
+    <v-btn
+      small
+      ripple
+      :disabled="isLastEvent"
+      @click="updateIndex(total - 1)"
+    >
+      |&gt;
+    </v-btn>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    total: {
+      type: [String, Number],
+      required: true,
+      default () { return 0 }
+    }
+  },
+  data () {
+    return {
+      current: 0
+    }
+  },
+  computed: {
+    isFirstEvent () {
+      return this.current === 0
+    },
+    isLastEvent () {
+      return this.current === this.total - 1
+    },
+    isRemainOlder () {
+      return !this.isFirstEvent
+    },
+    isRemainNewer () {
+      return !this.isLastEvent && this.total - this.current > 0
+    }
+  },
+  methods: {
+    updateIndex(index) {
+      this.$emit('updated-index', index)
+    }
+  }
 }
 </script>
 
